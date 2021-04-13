1025220f87ff200b3932b34267ce23d8baecce52
george-shammar <george.gbenle@outlook.com>
2021-04-13 11:40:23 +0000

    Create addBookToLibrary function to add new books to the library

--- assets/js/library.js
+++ assets/js/library.js
@@ -1,8 +1,25 @@
 let bookForm = document.querySelector('.form');
 let addNewBook = document.querySelector('button');
 
-// Display new book form after clicking Add new book button
+// Event listener for clicking Add new book button to dsipaly form.
 addNewBook.onclick = function () {
     bookForm.style.display = 'block';
 };
 
+// Book object constructor
+function Book(title, author, pages, read=false) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+};
+
+// Library array
+
+let myLibrary = [];
+
+// Add new book to Library array
+
+function addBookToLibrary(newBook) {
+    myLibrary.push(newBook);
+}
